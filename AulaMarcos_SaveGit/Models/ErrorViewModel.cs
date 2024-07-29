bd2c058f7816d7bd34d60d68a8f404ed7af30d38
RauanP <rauan.polli@icloud.com>
2024-07-29 16:46:54 -0300

    Atividade

---
+++ AulaMarcos_SaveGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AulaMarcos_SaveGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
